d42debb249b8b4537213b191dd4fa690b571984e
Ahren Swett <ahrenswett@Ahrens-MacBook-Pro.local>
2021-01-25 11:23:13 -0800

    Refactor to add Live Edge detection use case added new activity for CameraX revised main activity in preperation for buttons to start diferent use cases for testing added support for https://github.com/hannesa2/LiveEdgeDetection

---
+++ app/src/main/java/com/example/cameraxpractice/PictureViewEdit.kt
@@ -0,0 +1,19 @@
+package com.example.cameraxpractice
+
+import android.graphics.Matrix
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.camera.core.ImageProxy
+import androidx.camera.view.PreviewView
+
+class PictureViewEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_picture_view_edit)
+    }
+//    take in the new picture
+//    display it
+//    set up image matrix
+//    register finger touches for corner
+
+}
